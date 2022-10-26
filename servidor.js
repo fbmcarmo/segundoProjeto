e4b141c4e3c43dbfec4f89c63ef850957f2874ef
LABORATORIO T18 <alunot18@acad.unifor.br>
2022-10-26 20:27:10 -0300

    Servidor atualizado

--- servidor.js
+++ servidor.js
@@ -1,9 +1,13 @@
 var express = require("express");
 
+var fs = require("fs");
+
 var app = express();
 
+var html = fs.readFileSync("index.html");
+
 app.get("/", function(request, response){
-    response.end();
+    response.end(html);
 });
 
 app.listen(8888, function(erro){
