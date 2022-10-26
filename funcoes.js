fb64e67f9427ebe9a814cffbafc0831f5e1ba389
LABORATORIO T18 <alunot18@acad.unifor.br>
2022-10-26 19:42:43 -0300

    Criando as funcoes

---
+++ funcoes.js
@@ -0,0 +1,24 @@
+var func1 = function soma (a, b) {
+    return a + b;
+}
+
+var func2 = function sub (a, b) {
+    return a - b;
+}
+
+var func3 = function multip (a, b) {
+    return a * b;
+}
+
+var func4 = function div (a, b) {
+    return a / b;
+}
+
+var func5 = function piso (a, b) {
+    return 
+}
+
+var fun6 = function teto (a, b) {
+    return
+}
+
